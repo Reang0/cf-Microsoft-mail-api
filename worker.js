bfda70c4eab013ebd526e2ef70d78e7f06fc7b56
Reang0 <13470809201@163.com>
2025-07-11 14:02:52 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 // KV Namespace 绑定
-const API_BASE_URL = 'https://my-email-eta.vercel.app';
+const API_BASE_URL = 'https://my-email-eta.vercel.app/';
 const allowedMailboxes = ['INBOX', 'Junk'];
 
 // Logo URL
